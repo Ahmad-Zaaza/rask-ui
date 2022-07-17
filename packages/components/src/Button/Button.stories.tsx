f9eb209e206b24206ff52aedb506c05bb919d01d
Ahmad-Zaaza <ahmad94.dev@gmail.com>
2022-07-17 17:41:42 +0400

    add monorepo structure, add storybook

---
+++ packages/components/src/Button/Button.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, Story } from "@storybook/react";
+import Button from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+} as Meta;
+
+const Template: Story = args => <Button {...args} />;
+
+export const Default = Template.bind({});
